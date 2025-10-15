af1bf51583d2641084f96f05e90253c7bbc53739
Cech Daniel <cech.dan02@gmail.com>
2025-10-15 17:56:31 +0200

    s

--- Program.cs
+++ Program.cs
@@ -3,22 +3,26 @@ using PhotoApp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=/data/photoapp.db"));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// AUTOMATICKÁ MIGRACE, aby vznikly tabulky pøi startu aplikace:
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
 
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Photos}/{action=Index}/{id?}");
